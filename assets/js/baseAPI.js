eff7d76c1d9a86ceda6254938dc89e5a627220a0
pandandan <1500209789@qq.com>
2020-08-07 13:08:55 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -5,5 +5,22 @@ $.ajaxPrefilter(function (options) {
     console.log(options.url);
     //在发起真正的ajax请求之前统一拼接请求的跟路径 
     options.url='http://ajax.frontend.itheima.net'+options.url;
+    // 统一为有权限的接口，设置headers 请求头 
+  if(options.url.indexOf('/my/') != -1){
+    options.headers={
+        Authorization:localStorage.getItem('token')||''
+    }
+  }
+  //全局统一挂在complete回掉函数
+  options.complete=function(res){
+      if(res.responseJSON.status ===1 && res.responseJSON.message==="身份认证失败！"){
+          // 1.强制清空token
+          localStorage.removeItem('token');
+          // 2.强制跳转到登陆页
+          location.href='/login.html';
+      }
+      
+  
+  }
     
 })
